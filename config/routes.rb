ee154431d55d623382d63488eaa97fe0e9bd9695
Marcus Spiegel <spiegel@uscreen.de>
2013-07-26 20:38:00 +0200

    rm 2.3.0 throws 'undefined method routes_draw'

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
-RedmineApp::Application.routes_draw do
+# RedmineApp::Application.routes_draw do
   match 'bugherd/plugin_version', :controller => 'bugherd', :action => 'plugin_version'
   match 'bugherd/project_list.:format', :controller => 'bugherd', :action => 'project_list'
   match 'bugherd/status_list.:format', :controller => 'bugherd', :action => 'status_list'
   match 'bugherd/priority_list.:format', :controller => 'bugherd', :action => 'priority_list'
   match 'bugherd/trigger_web_hook/:project_id.:format', :controller => 'bugherd', :action => 'trigger_web_hook'
   match 'bugherd/issue/update.:format', :controller => 'bugherd', :action => 'update'
-  match 'bugherd/issue/add_comment.:format', :controller => 'bugherd', :action => 'add_comment'  
-end
+  match 'bugherd/issue/add_comment.:format', :controller => 'bugherd', :action => 'add_comment'
+# end
